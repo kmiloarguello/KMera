1080e7c4130759d18aae1e24f7aef4a930e09520
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2016-10-05 01:52:31 +0000

    getter and setter

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,10 +1,16 @@
 // JS
 /*  global $ */
 $(function () {
+    //metodo getter
     var a = $('<a>', {
         href: 'http://camiloarguello.co',
         target: '_blank',
         html: 'Ir a CamiloArguello.Co'
     })
     $('#app-body').append(a);
+    //metodo setter
+    a.attr('href','http://google.ca')
+    a.html("go ahead")
+    
+    
 });
