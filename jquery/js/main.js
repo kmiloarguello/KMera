b98c43921a54eb67cb77998608eb93c369031c99
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2016-10-05 01:56:57 +0000

    Add and Remove Class

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,16 +1,8 @@
 // JS
 /*  global $ */
 $(function () {
-    //metodo getter
-    var a = $('<a>', {
-        href: 'http://camiloarguello.co',
-        target: '_blank',
-        html: 'Ir a CamiloArguello.Co'
-    })
-    $('#app-body').append(a);
-    //metodo setter
-    a.attr('href','http://google.ca')
-    a.html("go ahead")
-    
-    
+    $('h1').addClass('danger');
+    setTimeout(function (){
+        $('h1').removeClass('danger');
+    },1500)
 });
