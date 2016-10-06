156cb5695f38280573cf133ec92bd1a0b933d436
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2016-10-05 22:15:40 -0500

    Añadiendo evento de busqueda por palabras clave

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,52 +1,72 @@
 // JS
 /*  global $ */
-$(function () {
-	
-    /**
-     * Submit form
-     **/
-     
-     $('#app-body')
-        .find('form')
-        .submit(function onsubmit(ev){
-            ev.preventDefault(); // Para que no se recargue la pantalla
-            var busqueda = $(this)
-                .find('input[type="text"]') // Devuelve el JQ Object
-                .val(); // LLamado del metodo val
-            alert('Se busco: ' + busqueda)
-                
-     })
-     
-	 var template =  '<article class="djs" alt=":img alt:">' +
-                    '<div class="left img-container">' +
-                        '<img src=":img:"></img>' +
-					'</div>' +
-                   ' <div class="right info"> ' +
-                        '<h1>:name:</h1>'+
-                        '<p>:summary:</p>' +
-                    '</div>' +
-                '</article>';
-	 
-    /** 
-    * REQUEST AJAX
-    **/
-	
-    $.ajax({
-        url: 'http://api.tvmaze.com/shows',
-        success: function (djs, textStatus, xhr){
-			
-			var $djsContainer = $('#app-body').find('div.main-djs');
-			$djsContainer.find('.loader').remove();
-			
-            djs.forEach(function (dj){
-				var article = template
-					.replace(':name:', dj.name)
-					.replace(':img:', dj.image.medium)
-					.replace(':summary:', dj.summary)
-					.replace(':img alt:', dj.name + " Logo")
+$(function() {
+  var $tvShowsContainer = $('#app-body').find('.main-djs');
 
-					$djsContainer.append($(article))
-			})
+  function renderShows(shows) {
+    $tvShowsContainer.find('.loader').remove();
+    shows.forEach(function (show) {
+      var article = template
+        .replace(':name:', show.name)
+        .replace(':img:', show.image ? show.image.medium : '')
+        .replace(':summary:', show.summary)
+        .replace(':img alt:', show.name + " Logo")
+
+      var $article = $(article)
+      $article.hide();
+      $tvShowsContainer.append($article.show());
+    })
+  }
+
+  /**
+   * Submit serach form
+   */
+
+  $('#app-body')
+    .find('form')
+    .submit(function (ev) {
+      ev.preventDefault();
+      var busqueda = $(this)
+        .find('input[type="text"]')
+        .val();
+
+      $tvShowsContainer.find('.djs').remove()
+      var $loader = $('<div class="loader">');
+      $loader.appendTo($tvShowsContainer);
+      $.ajax({
+        url: 'http://api.tvmaze.com/search/shows',
+        data: { q: busqueda },
+        success: function (res, textStatus, xhr) {
+          $loader.remove();
+          var shows = res.map(function (el) {
+            return el.show;
+          })
+
+          renderShows(shows);          
         }
-    });
-});
+      })
+    })
+
+  var template = '<article class="djs">' +
+          '<div class="left img-container">' +
+            '<img src=":img:" alt=":img alt:">' +
+          '</div>' +
+          '<div class="right info">' +
+            '<h1>:name:</h1>' +
+            '<p>:summary:</p>' +
+          '</div>' +
+        '</article>';
+
+  if (!localStorage.shows) {
+    $.ajax('http://api.tvmaze.com/shows')
+      .then(function (shows) {
+        $tvShowsContainer.find('.loader').remove();
+        localStorage.shows = JSON.stringify(shows);
+        renderShows(shows);
+      })
+  } else {
+    renderShows(JSON.parse(localStorage.shows));
+  }
+  
+})
+
