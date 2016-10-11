3f20c1e28f986c39d1933c097f3b708326477fd0
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2016-10-11 05:52:28 +0000

    Añadiendo dependencias SASS y MATERIALIZE con GULP

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var app = express();
 app.get('/', function (req, res){
     res.send('Hola Camilo');
 })
-
-app.listen(3000, function (err){
-    if(err) return console.log("Huubo error"), process.exit(1);
+// Create localhost server
+// app.listen(3000, function (err){
+//     if(err) return console.log("Huubo error"), process.exit(1);
     
-    console.log('Server on 3000')
-})
+//     console.log('Server on 3000')
+//})
+
+
+//Create server on cloud 9
+app.listen(process.env.PORT, process.env.IP);
