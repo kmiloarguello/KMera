8799abf64f824660cafcbfae3f86198251771da0
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2016-10-13 19:26:29 +0000

    Subiendo un archivo y guardarlo en una carpeta del servidor

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require("express"); //busca dentro de node_modules -> Express y trae main
+var multer  = require('multer');
+var ext = require("file-extension");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname))
+  }
+})
+
+var upload = multer({ storage: storage }).single('picture');
 
 var app = express();
 
@@ -45,6 +58,15 @@ app.get('/api/pictures', function (req, res, next) {
     
     setTimeout(function(){ res.send(pictures); },2000);
     
+});
+
+app.post('/api/pictures', function(req, res){
+    upload(req, res, function(err){
+        if (err){ 
+            return res.send(500, "Error uploading file");
+        }
+        res.send('File uploaded');
+    })
 })
 
 // Create localhost server
